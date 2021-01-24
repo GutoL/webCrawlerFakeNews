5a2987833c184fd5a8b5ec27da44e03ed345ec8f
Guto Leoni <guto-acmilan@hotmail.com>
2021-01-24 14:51:07 -0300

    getting number of pages dynamically and considering different versions of fake news

--- README.md
+++ README.md
@@ -4,8 +4,10 @@ Web crawler escrito em python para extração de fake news sobre a COVID-19 no s
 O crawler busca as informações sobre fake news do site e salva um data frame com as seguintes informações das fake news:
 
 *Data: Data em que a notícia foi publicada no site;<br>
+*URL: url para a postagem sobre a fake news;<br>
 *Texto: texto que descreve a notícia;<br>
 *Fonte: mostra onde a notícia estava sendo publicada (whatsapp, facebook, etc.);<br>
+*classificação: o site lupa, ao avaliar cada notícia, as classifica em diferentes categorias (falso, exagerado, contraditório, etc).
 *Link da imagem: as notícias eventualmente são publicadas em formato de imagem nas redes sociais, esse campo mostra o link para a referida imagem; e<br>
 *JSON: JSON com o resumo de toda as informações que são publicadas no site
 
--- crawler.py
+++ crawler.py
@@ -18,66 +18,107 @@ def find_links_from_search_page(url):
     selector = 'h2.bloco-title > a' # piaui lupa
     soup = get_soup(url)
     links = [a['href'] for a in soup.select(selector)]
-    return links
+
+    portuguese_links = []  # there are some links with spanish news, we remove them
+    for l in links:
+      if re.search("verificamos-", l) != None:
+        portuguese_links.append(re.search("verificamos-", l).string)
+    
+    return portuguese_links
 
 def scrape_search_for_links(page_index):
-    print('Scrapping page', page_index)
     links = find_links_from_search_page(site + search_query + '/page/' +
-                                        str(page_index))
-    print('> Found', len(links), 'links for page', page_index)
+                                        str(page_index)+'/')
     return links
-
-def scrape_fake(link):
-    print('> Scrapping', link)
-    soup = get_soup(link)
-
-    contents = soup.find_all('script', type='application/ld+json')
-    
-    if len(contents) < 3:
-        print("Error: JSON is not present in the page...")
-        return [None]*6
-
-    json_data = str(contents[2]) # because there are 3 scripts per post, and the last one has all information of fake news
-    
-    date, text, source, json_data = get_data_from_json(json_data)
-
-    links = soup.findAll('img')
-    image_link = links[6]['src'] # because the page has several images, and the 6° is related to the fake news
-    
-    return date, link, text, source, json_data, image_link
 
 def get_data_from_json(json_data):
     json_data = json_data.replace('<script type="application/ld+json">','')
     json_data = json_data.replace('</script>','')
     # json_data = json_data.replace('”','"')
     
-    # date: datePublished
     date = re.search('"datePublished":(.*),', json_data).group(1)
+    
+    classification = re.search('"alternateName":(.*),', json_data).group(1).replace('"','')
     
     # # url: url
     # url = re.search('"url":(.*),', json_data).group(1)
     
-    # text: "claimReviewed"
     text = re.search('"claimReviewed":(.*),', json_data).group(1)
 
     # source: itemReviewed -> name
     source = re.findall('"name":(.*)\r', json_data)[1].replace(',','') # the first one is the name of site (Agência Lupa)
     
-    return date, text, source, json_data
+    return date, text, source, classification, json_data
+
+def scrape_fake(link):
+    print('> Scrapping', link)
+    soup = get_soup(link)
+
+    links = soup.findAll('img')
+    image_link = links[6]['src'] # because the page has several images, and the 6° is related to the fake news
+
+    contents = soup.find_all('script', type='application/ld+json')
+    
+    if len(contents) < 3:
+        print("Error: JSON is not present in the page...")
+        return [None]*6
+    
+    json_list = []
+
+    for x in range(2,len(contents)): # because the first two scripts [0,1] are not related to fake news
+        json_list.append(str(contents[x]))
+
+    date_list = []
+    text_list = []
+    source_list = []
+    json_data_list = []
+    classification_list = []
+
+    for x in range(len(json_list)):
+        date, text, source, classification, json_data = get_data_from_json(json_list[x])
+        
+        date_list.append(date)
+        text_list.append(text)
+        source_list.append(source)
+        classification_list.append(classification)
+        json_data_list.append(json_data)
+
+    image_link_list = [image_link] * len(date_list)
+    link_list = [link] * len(date_list)
+    
+    return date_list, link_list, text_list, source_list, classification_list, json_data_list, image_link_list
+
+
     
     
 
 site = "https://piaui.folha.uol.com.br/lupa/"
-search_query = '/tag/verificamoscovid/'
+search_query = 'tag/verificamoscovid/'
 
 if __name__ == "__main__":
     initial_page = 1
-    final_page = 1
+    final_page = -1 # -1 → to search in all pages
     print('Start scrapping', site, 'from page', initial_page, 'to', final_page)
     
     all_links = []
-    for number_page in range(initial_page, final_page+1):
-        all_links.extend(scrape_search_for_links(number_page))
+    search = True
+    
+    number_page = initial_page
+    while search:
+        if (number_page == final_page+1):
+            break
+        
+        print('Scrapping page', number_page)
+        links = scrape_search_for_links(number_page)
+
+        if (len(links) > 10): # is a page with no fake news, stop!
+            break
+        else:
+            print('> Found', len(links), 'links for page', number_page)
+            all_links.extend(links)
+        
+        number_page += 1
+
     cont_links = len(all_links)
     print('> Found', cont_links, 'total')
 
@@ -86,33 +127,25 @@ if __name__ == "__main__":
     fail = 0
 
     for link in all_links:
-        date, url, text, source, json_data, image_link = scrape_fake(link)
+        date_list, link_list, text_list, source_list, classification_list, json_data_list, image_link_list = scrape_fake(link)
         
-        if json_data is not None:
-            all_fakes.append({
-                'date':date, 
-                'url':url,
-                'text': text,
-                'source':source,
-                'json':json_data,
-                'img':image_link
-                })
+        if json_data_list is not None:
+            for x in range(len(date_list)):
+                all_fakes.append({
+                    'date':date_list[x], 
+                    'url':link_list[x],
+                    'text': text_list[x],
+                    'source':source_list[x],
+                    'classification':classification_list[x],
+                    'json':json_data_list[x],
+                    'img':image_link_list[x]
+                    })
         else:
             fail+=1
 
     print('Sucess:',len(all_links)-fail,'Fail:',fail,'Total:',len(all_links))
-    rows = []
     
-    for fake in all_fakes:
-        rows.append({
-            'date':fake['date'], 
-            'url':fake['url'],
-            'text': fake['text'],
-            'source':fake['source'],
-            'json':fake['json'],
-            'img':fake['img']
-         }) 
     
-    df = pd.DataFrame(rows)
-    df.to_csv('data.csv',sep=';')
+    df = pd.DataFrame(all_fakes)
+    df.to_csv('data.csv',sep=';') #'''
 
